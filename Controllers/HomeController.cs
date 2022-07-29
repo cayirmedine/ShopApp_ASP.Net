0cd135ba289f5665ae1cbaa8a44557d110a10519
cayirmedine <htc.mdn.2010@gmail.com>
2022-07-29 11:01:29 +0300

    Project init, basic controllers added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "home/index";
+        }
+
+        public string About()
+        {
+            return "home/about";
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopApp.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public string Index()
+        {
+            return "product/index";
+        }
+
+        public string List()
+        {
+            return "product/list";
+        }
+
+        public string Details(int id)
+        {
+            return "product/details/"+id;
+        }
+    }
+}
