41f4ce7e8aa10d0f420f4efcfdeecd7ebc48e5ba
cayirmedine <htc.mdn.2010@gmail.com>
2022-08-01 11:42:55 +0300

    View component added for categories

---
+++ ViewComponents/CategoriesViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopApp.WebUI.Models;
+
+namespace ShopApp.WebUI.ViewComponents
+{
+    public class CategoriesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var categories = new List<Category>()
+            {
+                new Category { Name="Phones", Description="Phone Category" },
+                new Category { Name="Computer", Description="Computer Category" },
+                new Category { Name="Electronic", Description="Electronic Category" }
+            };
+
+            return View(categories);
+        }
+    }
+}
