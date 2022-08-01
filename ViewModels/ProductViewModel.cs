476358acdd7e717405fac666c0cf34ab85dea97b
cayirmedine <htc.mdn.2010@gmail.com>
2022-08-01 11:43:52 +0300

    Categories removed from product view model

--- ViewModels/ProductViewModel.cs
+++ ViewModels/ProductViewModel.cs
@@ -8,8 +8,6 @@ namespace ShopApp.WebUI.ViewModels
 {
     public class ProductViewModel
     {
-        public List<Category> Categories { get; set; }
-
         public List<Product> Products { get; set; }
     }
 }
