9fd875d8a03e2e6d4863be3331a46416bfea2755
cayirmedine <htc.mdn.2010@gmail.com>
2022-08-02 14:56:02 +0300

    id attribute added to category and product models

--- Models/Category.cs
+++ Models/Category.cs
@@ -7,6 +7,7 @@ namespace ShopApp.WebUI.Models
 {
     public class Category
     {
+        public int CategoryId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
--- Models/Product.cs
+++ Models/Product.cs
@@ -7,11 +7,15 @@ namespace ShopApp.WebUI.Models
 {
     public class Product
     {
+        public int ProductId { get; set; }
+
         public string Name { get; set; }
 
         public double Price { get; set; }
 
         public string Description { get; set; }
+
+        public string ImageUrl { get; set; }
 
         public bool isApproved { get; set; }
     }
