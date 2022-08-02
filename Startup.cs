e523d14ee8d75a74c527bb2567439b433f5301fd
cayirmedine <htc.mdn.2010@gmail.com>
2022-08-02 11:29:30 +0300

    Static files added to project

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,8 @@ namespace ShopApp.WebUI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseStaticFiles(); //accessable wwwroot 
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
